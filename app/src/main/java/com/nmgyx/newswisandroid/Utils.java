d96914259ba1801abef0695c62576bbe22cf4de7
houjianjiang <houjianjiang@q.com>
2024-11-11 15:35:09 +0800

    Initial commit

---
+++ app/src/main/java/com/nmgyx/newswisandroid/Utils.java
@@ -0,0 +1,20 @@
+package com.nmgyx.newswisandroid;
+
+import android.util.Log;
+
+/**
+ * Description:
+ * Created by Android Studio.
+ * User: houjianjiang
+ * Date: 2024/11/11
+ * Time: 15:32
+ */
+public class Utils {
+
+    public Utils() {
+    }
+
+    public static void show(){
+        Log.e("+++++", "++++");
+    }
+}
